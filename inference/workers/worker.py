a6faec163cf8b3c38c6fdac4458d6fc5b0207f50
Debankur04 <debankurdutta04@gmail.com>
2026-01-22 14:07:36 +0530

    first commit

--- inference/workers/worker.py
+++ inference/workers/worker.py
@@ -12,6 +12,7 @@
 from redis import Redis
 from workers import email, image_prep, pdf_creator, prediction
 from supabase.supabase_init import supabase
+import json
 
 r = Redis(
     host="localhost",
@@ -25,6 +26,42 @@ QUEUE_NAME = "task_queue"
 while True:
     task_data = r.blpop(QUEUE_NAME, timeout=0)
     if task_data:
+
+        { "job_id": "b7f1c0b2-9e4f-4a9d-9d6f-8a2a9f71c123", 
+        "user_id": "u_123456", 
+        "user_email": "user@example.com", 
+        "bucket": "user-uploads", 
+        "input_prefix": "users/u_123456/jobs/b7f1c0b2/input/", 
+        "manifest_path": "users/u_123456/jobs/b7f1c0b2/manifest.json", 
+        "report_prefix": "users/u_123456/jobs/b7f1c0b2/report/" }
+
         bucket = task_data['bucket']
         input_prefix = task_data['input_prefix']
         manifest_path = task_data['manifest_path']
+        results = []
+        batch_images = []
+
+        manifest = (
+        supabase.storage
+        .from_(bucket)
+        .download(manifest_path))
+        with open(manifest, 'r') as file:
+            data = json.load(file)
+        
+        for relative_path in data['images']:
+            image = image_prep(bucket = bucket, file_path = relative_path)
+            batch_images.append(image)
+            if len(batch_images) == 2:
+                batch_result = prediction.predict_batch(batch_images)
+                results.extend(batch_result)
+                batch_images.clear()
+        if batch_images == 1:
+            batch_result = prediction.predict_batch(batch_images)
+            results.extend(batch_result)
+            batch_images.clear()
+        pdf_creator.create_pdf_report(results= results)
+        response = (
+        supabase.storage
+        .from_(bucket)
+        .remove(input_prefix)
+        )
